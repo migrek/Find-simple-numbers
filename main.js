76faa15995fb7eba43fdfa3c4869d54fc3ba725b
Ekaterina Grudinskaya <40333436+migrek@users.noreply.github.com>
2018-10-24 20:42:22 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+//to find simple numbers in range
+let userNumber = parseInt(prompt('Insert the number: '));
+let arr = [];
+
+if (userNumber === 0 || userNumber === 1) {
+    console.info('No simple numbers');
+}
+else {
+    for (let i=2; i <= userNumber; i++) {
+        let isSimple = true;
+        for (let j=2; j < i; j++) {
+			if (i % j === 0) {
+                isSimple = false;}
+            }
+        if (isSimple) {
+            arr.push(i);
+        }     
+};
+console.info(arr);}
+ 
+
